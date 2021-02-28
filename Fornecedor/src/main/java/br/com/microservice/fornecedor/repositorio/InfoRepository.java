e7601878325e7b9d8ba38faed89a7d7029f9f308
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-02-28 20:59:44 -0300

    Projeto Fornecedor que acessa a base de dados

---
+++ Fornecedor/src/main/java/br/com/microservice/fornecedor/repositorio/InfoRepository.java
@@ -0,0 +1,14 @@
+package br.com.microservice.fornecedor.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.microservice.fornecedor.model.InfoFornecedor;
+
+@Repository
+public interface InfoRepository extends CrudRepository<InfoFornecedor, Long  > {
+
+	InfoFornecedor findByEstado(String estado);
+	
+	
+}
